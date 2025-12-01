0e0adfed79c46b55872d12116d20f75ffe4453a8
Mohamed Usama <mohamedusama421@gmail.com>
2025-12-01 10:46:25 +0200

    feat(upload): implement image upload (single/multiple) with memory storage and sharp resizing

---
+++ middlewares/uploadImageMiddleware.js
@@ -0,0 +1,22 @@
+import multer from "multer";
+import ApiError from "../utils/apiError.js";
+
+const multerOption = () => {
+  const multerStorage = multer.memoryStorage();
+
+  const multerFilter = function (req, file, cb) {
+    if (file.mimetype.startsWith("image")) {
+      cb(null, true);
+    } else {
+      cb(new ApiError("ONLY image allowed", 400), false);
+    }
+  };
+  const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
+  return upload;
+};
+
+export const uploadSingleImage = (FieldName) =>
+  multerOption().single(FieldName);
+
+export const uploadMixOfIamges = (arrayOfFeilds) =>
+  multerOption().fields(arrayOfFeilds);
