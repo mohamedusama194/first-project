6c52dfc04fd2c1de6d3709d94b1bca9788be8e3b
Mohamed Usama <mohamedusama421@gmail.com>
2025-12-06 23:08:41 +0200

    refactor(brand): streamline image processing logic in resizeImage function

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -13,34 +13,31 @@ import {
 import ensureFolderExists from "../utils/createFolder.js";
 // image the name of field in model
 export const uploadBrandImage = uploadSingleImage("image");
-
 export const resizeImage = asyncHandler(async (req, res, next) => {
   const filename = `brand-${uuidv4()}-${Date.now()}.jpeg`;
-  await sharp(req.file.buffer) //image processing for node
-    .resize(600, 600)
-    .toFormat("jpeg")
-    .jpeg({ quality: 80 })
-    .toFile(`${ensureFolderExists("uploads/brands")}/${filename}`);
-  //save image into db
-  req.body.image = filename;
+  if (req.file) {
+    await sharp(req.file.buffer) //image processing for node
+      .resize(600, 600)
+      .toFormat("jpeg")
+      .jpeg({ quality: 80 })
+      .toFile(`${ensureFolderExists("uploads/brands")}/${filename}`);
+    //save image into db
+    req.body.image = filename;
+  }
   next();
 });
 // get all brand
 //@route get /api/v1/brands
 export const getAllBrands = getAll(brandModel);
-
 // get spicific brand
 //@route get /api/v1/brands/:id
 export const getBrand = getOne(brandModel);
-
 // Create brand
 //@route POST /api/v1/brands
 export const createBrand = createOne(brandModel);
-
 // Update brand
 //@route put /api/v1/brands/:id
 export const updateBrand = updateOne(brandModel);
-
 // delete brand
 //@route delete /api/v1/brands/:id
 export const deleteBrand = deleteOne(brandModel);
